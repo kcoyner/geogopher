f1003b301144217fb80ac6a7727c46a7902f08ff
Kevin Coyner <rustybear@gmail.com>
2017-11-23 13:04:48 -0500

    Added tests using shallow rendering.

--- test/components/GamesList.test.js
+++ test/components/GamesList.test.js
@@ -5,13 +5,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GamesList from '../../src/components/GamesList';
+import { createRenderer } from 'react-test-renderer/shallow';
 
 global.requestAnimationFrme = function(callback) {
   setTimeout(callback, 0);
 };
 
-it('renders GamesList component', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<GamesList />, div);
+describe('The games list', () => {
+
+  /* These first two tests are essentially the same. The test using
+   * createRenderer does a shallow rendering, which means that children of
+   * the component will not be rendered.  Use TestUtils instead.
+   */
+
+  it('renders GamesList component', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<GamesList />, div);
+  });
+
+  it('renders a GamesList component', () => {
+    const gamesRenderer = createRenderer();
+    gamesRenderer.render(<GamesList/>);
+    let g = gamesRenderer.getRenderOutput();
+    expect(g.type).toBe('div');
+  });
+
 });
+
 
--- test/components/Map.test.js
+++ test/components/Map.test.js
@@ -5,19 +5,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Map from '../../src/components/Map';
+import { createRenderer } from 'react-test-renderer/shallow';
 
 global.requestAnimationFrme = function(callback) {
   setTimeout(callback, 0);
 };
 
 describe('the map', () => {
+
+  /* These first two tests are essentially the same. The test using
+   * createRenderer does a shallow rendering, which means that children of
+   * the component will not be rendered.  Use TestUtils instead.
+   */
+
   it('renders Map div', () => {
     const div = document.createElement('div');
     ReactDOM.render(<maps />, div);
   });
 
+  it('renders a Map component', () => {
+    const mapRenderer = createRenderer();
+    mapRenderer.render(<Map/>);
+    let m = mapRenderer.getRenderOutput();
+    expect(m.type).toBe('div');
+  });
+
   it('has title Geogophers', () => {
     expect.stringContaining('Geogophers');
   });
+
 });
 
