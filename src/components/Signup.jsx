5c69a44d1c0f35fac1f05620d5d8c3f53e055eb6
Jessica D'Andrea <jdandrea11@gmail.com>
2017-11-29 19:43:45 -0600

    Add dotenv and begin sign up form

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,15 +1,75 @@
-import { Input } from 'semantic-ui-react';
 import React from 'react';
+import { Button, Checkbox, Form } from 'semantic-ui-react';
 
-const Signup = () => {
-  return (
-    <div>
-      <h1>Sign up</h1>
-      <Input placeholder='Email' />
-      <Input placeholder='Password' />
-      <Input placeholder='Confirm Your Password' />
-    </div>
-  );
-};
+class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        username: '',
+        password: '',
+        firstName: '',
+        lastName: '',
+        email: '',
+      }
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    const { user } = this.state;
+    this.setState({
+        user: {
+            ...user,
+            [name]: value
+        }
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.setState({ submitted: true });
+    const { user } = this.state;
+    const { dispatch } = this.props;
+    if (user.firstName && user.lastName && user.username && user.password && user.email) {
+        console.log(user);
+        // dispatch(userActions.register(user));
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Sign up</h1>
+          <Form onChange={this.handleChange} onSubmit={this.handleSubmit}>
+            <Form.Field>
+              <label>First Name</label>
+              <input name='firstName' placeholder='First Name' required='true'/>
+            </Form.Field>
+            <Form.Field>
+              <label>Last Name</label>
+              <input name='lastName' placeholder='Last Name' required='true'/>
+            </Form.Field>
+            <Form.Field>
+              <label>Email</label>
+              <input name='email' placeholder='Email' required='true'/>
+            </Form.Field>
+            <Form.Field>
+              <label>Password</label>
+              <input name='password' placeholder='Password' type='password'required='true'/>
+            </Form.Field>
+            <Form.Field>
+              <label>Username</label>
+              <input name='username' placeholder='Username' required='true'/>
+            </Form.Field>
+            <Button type='submit'>Submit</Button>
+        </Form>
+      </div>
+    );
+  }
+}
 
 export default Signup;
