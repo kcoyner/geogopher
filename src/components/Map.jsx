bcd47a11a166ba269be00718c5ec183dec4b2f20
nfd <nickfreemandesign>
2017-12-04 14:33:58 -0600

    added lightweight documentation to map component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -26,6 +26,7 @@ export default class Map extends React.Component {
   }
 
   componentDidMount() {
+    //initialize new google map and place it on '#map'
     map = new window.google.maps.Map(document.getElementById('map'), {
       zoom: 2,
       center: {
@@ -39,6 +40,7 @@ export default class Map extends React.Component {
       streetViewControl: false,
       mapTypeControl: false,
       mapTypeId: 'roadmap',
+      //turn off all country names and labels
       styles: [
         {
           featureType: "all",
@@ -51,22 +53,24 @@ export default class Map extends React.Component {
         }
       ]
     });
-
+    //load in coordinate data with country name information
     map.data.loadGeoJson(
       'https://s3.amazonaws.com/gopher-geofiles/geogophers-mvp-world-countries.json');
+    //set all loaded coordinate data to a red fill color with no stroke
     map.data.setStyle({
       fillColor: 'red',
       strokeWeight: '0'
     });
 
-    this.nameInput.focus();
   }
+  //on quit, set change game state and clear timer
   handleQuit() {
     this.setState({
       quit: true,
       gameOver : true });
     clearInterval(this.incrementer);
   }
+  //on start focus client cursor to answerInput field and start timer?
   handleStart() {
         this.nameInput.focus();
         this.setState({ gameStart: false });
@@ -76,22 +80,23 @@ export default class Map extends React.Component {
         })
       , 1000);
     }
+  //closes gameStart modal onClick
   handleClose(){
     this.setState({ open: false });
     this.props.history.push('/');
   }
-
+  //regisers inputs entered into input field
   onInputChange(e) {
-    this.setState({
-      inputValue: e.target.value
-    })
+    this.setState({ inputValue: e.target.value })
   }
+  //resets timer and returns gameOver modal
   isEnd() {
     if(this.state.secondsElapsed === 0) {
       clearInterval(this.incrementer);
       return <GameOver onClose={ this.handleClose } open={this.state.gameOver}/>
     }
   }
+  //keypress checks if key is 'enter'. registers value against answers and clears input
   keyPress(e) {
     if(e.keyCode == 13){
         let answerInputted = e.target.value;
