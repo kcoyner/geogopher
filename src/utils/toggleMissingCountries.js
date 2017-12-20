8d76c9965f3b2ebb4d283e7edd68177bc2da200c
nfd <nicho.freeman@gmail.com>
2017-12-20 15:15:53 -0800

    added custom markers

--- src/utils/toggleMissingCountries.js
+++ src/utils/toggleMissingCountries.js
@@ -1,6 +1,19 @@
+const imgFromAssets = require('-!file-loader?name=markerImg!../assets/geogopher-marker.png');
+const imgFromAssets2 = require('-!file-loader?name=markerImg!../assets/geogopher-marker-2.png');
+
 export const toggleMissingCountries = (toggle, gameData, map) => {
   //declare empty array of missing countries
   let missingCountries = [];
+
+  var geogopherMarker = {
+    url: imgFromAssets,
+    // This marker is 20 pixels wide by 32 pixels high.
+    size: new google.maps.Size(20, 32),
+    // The origin for this image is (0, 0).
+    origin: new google.maps.Point(0, 0),
+    // The anchor for this image is the base of the flagpole at (0, 32).
+    anchor: new google.maps.Point(0, 32)
+  };
   //loop through gameData
   gameData.forEach((el) => {
     //if polygonAnswered === false
@@ -8,6 +21,7 @@ export const toggleMissingCountries = (toggle, gameData, map) => {
       //create a marker (this will auto render on map)
         let marker = new google.maps.Marker({
           map: map,
+          icon: imgFromAssets2,
           draggable: false,
           animation: null,
           position: {lat: el.countryCenter[0], lng: el.countryCenter[1]}
