2be5ac0799832a22f2e13dc6cf9e4ed249e6b206
Jessica D'Andrea <jdandrea11@gmail.com>
2018-01-06 17:45:43 -0600

    Add time remaining to scores page, format seconds, fix async issue

--- src/components/HighScores.jsx
+++ src/components/HighScores.jsx
@@ -6,6 +6,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
 import { Icon, Label, Menu, Table, Dropdown } from 'semantic-ui-react';
+import { capitalizeWords } from '../utils/capitalizeFirst';
+import moment from 'moment';
+import momentDurationFormatSetup from 'moment-duration-format';
 
 class HighScores extends React.Component {
   constructor(props) {
@@ -19,7 +22,9 @@ class HighScores extends React.Component {
         selectedDifficulty: Number(props.match.params.gameDiffId) || 1,
         games: null,
         gameTypes: null,
-        gameDifficulties: null
+        gameDifficulties: null,
+        total: null,
+        page: 1
     }
     this.getScores = this.getScores.bind(this);;
     this.getGameAttributes = this.getGameAttributes.bind(this);
@@ -28,23 +33,32 @@ class HighScores extends React.Component {
 
     componentDidMount() {
       this.getGameAttributes();
-      this.getScores();
+
+
   }
 
   onChange(e, data) {
       const obj = {}
       obj[data.name] = data.value;
-      this.setState(obj, this.getScores)
+      this.setState(obj, this.getScores);
+      console.log(data);
+
   }
   getGameAttributes() {
       axios.get('/api/gameslist')
       .then(response => {
           let games = [];
           response.data.forEach(element => {
-              let game = Object.assign({}, { value: element.game_id, text: element.game_name });
+              let values = {};
+              values['game_id'] = element.game_id;
+              values['max_count_polygons'] = element.max_count_polygons;
+              let game = Object.assign({}, { value: element.game_id, text: element.game_name, total: element.max_count_polygons });
               games.push(game);
           })
-          this.setState({ games: games });
+
+          this.setState({
+              games: games
+            });
       })
       axios.get('/api/gameSettings')
       .then(response => {
@@ -55,13 +69,15 @@ class HighScores extends React.Component {
                 difficulties.push(difficulty);
           });
           response.data.game_types.forEach(element => {
-            let type = Object.assign({}, { value: element.game_type_id, text: element.game_type_name });
+            const gameName = capitalizeWords(element.game_type_name)
+            const type = Object.assign({}, { value: element.game_type_id, text: gameName });
             types.push(type);
           })
           this.setState({
               gameDifficulties: difficulties,
               gameTypes: types
           })
+          this.getScores();
       })
   }
 
@@ -85,9 +101,9 @@ class HighScores extends React.Component {
   render() {
     return (
       <div>
-          <Dropdown name="selectedGameType" value={this.selectedGameType} onChange={this.onChange} defaultValue={this.state.selectedGameType} fluid selection options={this.state.gameTypes} />
-          <Dropdown name="selectedGame" value={this.selectedGame} onChange={this.onChange} defaultValue={this.state.selectedGame} fluid selection options={this.state.games} />
-          <Dropdown name="selectedDifficulty" value={this.selectedDifficulty} onChange={this.onChange} defaultValue={this.state.selectedDifficulty} fluid selection options={this.state.gameDifficulties} />
+          <Dropdown name="selectedGameType" onChange={this.onChange} defaultValue={this.state.selectedGameType} fluid selection options={this.state.gameTypes} />
+          <Dropdown name="selectedGame" onChange={this.onChange} value={this.state.selectedGame} fluid selection options={this.state.games} />
+          <Dropdown name="selectedDifficulty" onChange={this.onChange} defaultValue={this.state.selectedDifficulty} fluid selection options={this.state.gameDifficulties} />
 
           { this.state.first ? (
 
@@ -96,18 +112,16 @@ class HighScores extends React.Component {
             <Table.Row>
                 <Table.HeaderCell>Place</Table.HeaderCell>
                 <Table.HeaderCell>Username</Table.HeaderCell>
-                <Table.HeaderCell>Time</Table.HeaderCell>
                 <Table.HeaderCell>Score</Table.HeaderCell>
-                <Table.HeaderCell>Hints Used</Table.HeaderCell>
+                <Table.HeaderCell>Time Remaining</Table.HeaderCell>
             </Table.Row>
             </Table.Header>
             <Table.Body>
                 <Table.Row>
                     <Table.Cell><Label ribbon>First</Label></Table.Cell>
                     <Table.Cell>{this.state.first.user.username }</Table.Cell>
-                    <Table.Cell>{this.state.first.game_timer_start - this.state.first.game_timer_remaining }</Table.Cell>
-                    <Table.Cell>{this.state.first.count_polygons_entered + "/" + this.state.first.count_polygons_entered + JSON.parse(this.state.first.polygons_unanswered).length }</Table.Cell>
-                    <Table.Cell>{this.state.first.count_total_hints }</Table.Cell>
+                    <Table.Cell>{this.state.first.count_polygons_entered + "/" + this.state.total }</Table.Cell>
+                    <Table.Cell>{ moment.duration(this.state.first.game_timer_remaining, "seconds").format() }</Table.Cell>
                 </Table.Row>
             {
                 this.state.scores.map((score, index) => (
@@ -115,9 +129,8 @@ class HighScores extends React.Component {
                 <Table.Row key={index}>
                     <Table.Cell>{index + 2}</Table.Cell>
                     <Table.Cell>{score.user.username}</Table.Cell>
-                    <Table.Cell>{score.game_timer_start - score.game_timer_remaining }</Table.Cell>
                     <Table.Cell>{score.count_polygons_entered + "/"}</Table.Cell>
-                    <Table.Cell>{score.count_total_hints }</Table.Cell>
+                    <Table.Cell>{ moment.duration(score.game_timer_remaining, "seconds").format() }</Table.Cell>
                 </Table.Row>
                 ))
             }
