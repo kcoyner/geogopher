6a2671b4cd276ed67e3b1f74ff017deae46975d1
Jessica D'Andrea <jdandrea11@gmail.com>
2018-01-07 15:54:37 -0600

    Add error messages to registration page

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -3,7 +3,7 @@
  */
 
 import React from 'react';
-import { Button, Checkbox, Form } from 'semantic-ui-react';
+import { Button, Checkbox, Form, Message } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 
 import { userActions } from '../actions';
@@ -18,7 +18,9 @@ class RegisterPage extends React.Component {
         firstName: '',
         lastName: '',
         email: '',
-      }
+      },
+      error: false,
+      errorMessage: null
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -43,8 +45,23 @@ class RegisterPage extends React.Component {
     const { dispatch } = this.props;
     if (user.firstName && user.lastName && user.username && user.password && user.email) {
         dispatch(await userActions.register(user))
-          .then(() => {
-            this.props.history.push('/');
+          .then(data => {
+            if(data){
+              console.log(data);
+              this.setState({
+                error: true,
+                errorMessage: data
+              })
+            } else {
+              this.props.history.push('/');
+            }
+          })
+          .catch(err => {
+            console.log(err);
+            this.setState({
+              error: true,
+              errorMessage: err
+            })
           })
     }
   }
@@ -54,28 +71,36 @@ class RegisterPage extends React.Component {
       <div>
         <h1>Register</h1>
           <Form onChange={this.handleChange} onSubmit={this.handleSubmit} >
-            <Form.Field>
+            <Form.Field required>
               <label>First Name</label>
               <input name='firstName' placeholder='First Name' required='true'/>
             </Form.Field>
-            <Form.Field>
+            <Form.Field required>
               <label>Last Name</label>
               <input name='lastName' placeholder='Last Name' required='true'/>
             </Form.Field>
-            <Form.Field>
+            <Form.Field required> 
               <label>Email</label>
-              <input name='email' placeholder='Email' required='true'/>
+              <input name='email' placeholder='Email' type='email' required='true'/>
             </Form.Field>
-            <Form.Field>
+            <Form.Field required>
               <label>Password</label>
               <input name='password' placeholder='Password' type='password'required='true'/>
             </Form.Field>
-            <Form.Field>
+            <Form.Field required>
               <label>Username</label>
               <input name='username' placeholder='Username' required='true'/>
             </Form.Field>
             <Button type='submit'>Submit</Button>
         </Form>
+        { this.state.error &&
+        <Message
+        error
+        header='Oops! There seems to be a problem'
+        content={this.state.errorMessage}
+      />
+      }
+        
       </div>
     );
   }
