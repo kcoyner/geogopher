92c64d935aae077104112c647fedbd97e4f13119
Kevin Coyner <rustybear@gmail.com>
2018-01-14 19:50:22 -0500

    Move testing DB server from AWS to Station-4.

--- scripts/reset_testing_db.sh
+++ scripts/reset_testing_db.sh
@@ -3,9 +3,9 @@
 if [ -f $HOME/.pgpass ]; then
   echo "` chmod 0600 $HOME/.pgpass `"
 
-  echo " ` psql -h geogophers-postgresql-db-test.c8nqtytgojtc.us-east-1.rds.amazonaws.com -p 5432 -U webAppLogin  geogophertesting  -f ./scripts/postgres_db_create.sql` "     
+  echo " ` psql -h stn4.homelinux.com -p 5432 -U webapplogin  geogophertesting  -f ./scripts/postgres_db_create.sql` "
 
-  echo " ` psql -h geogophers-postgresql-db-test.c8nqtytgojtc.us-east-1.rds.amazonaws.com -p 5432 -U webAppLogin  geogophertesting  -f ./scripts/postgres_db_insert_sample_data.sql` "     
+  echo " ` psql -h stn4.homelinux.com -p 5432 -U webapplogin  geogophertesting  -f ./scripts/postgres_db_insert_sample_data.sql` "
 
 else
    echo "You need a .pgpass file in your home directory with proper DB
